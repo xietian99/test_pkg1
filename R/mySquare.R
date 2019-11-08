83f9a18d4eb2a453e6822a38fbbd78412dafef58
xiet <xiet@umich.edu>
2019-11-08 16:20:32 -0500

    "added updated files"

---
+++ R/mySquare.R
@@ -0,0 +1,14 @@
+#'Square
+#'
+#'Gets the square of a number #'
+#'@param x input value
+#'
+#'@return the square of x
+#'
+#'@examples
+#'square(4)
+#'
+#'@export
+#'
+
+square = function(X) return(X*X)
